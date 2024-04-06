10bb09a1f4ed0ba998060a0a4ffa191ef856b956
Chris Chiu <cchiu65@gatech.edu>
2024-04-06 14:27:14 +1100

    location fix

--- cardiacmap/assets/drag_handler.js
+++ cardiacmap/assets/drag_handler.js
@@ -53,7 +53,7 @@ window.dash_clientside.clientside = {
             const minWidth = Math.min(rect.width, rect.height);
 
             // Calculate offset within the actual graph element
-            const offsetX = moveEvent.clientX - rect.left - WINDOW_OFFSET / 2;
+            const offsetX = moveEvent.clientX - rect.left - (rect.width + WINDOW_OFFSET - minWidth) / 2;
             const offsetY = moveEvent.clientY - rect.top - (rect.height + WINDOW_OFFSET - minWidth) / 2;
 
             console.log(offsetX, offsetY);
