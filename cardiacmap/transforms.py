adaaef5086a432fcb7739c4c7833435e2e463d46
slenka7 <slenka7@gatech.edu>
2024-04-09 20:49:16 +0530

    add fft function

--- cardiacmap/transforms.py
+++ cardiacmap/transforms.py
@@ -450,3 +450,19 @@ def NormalizeData(data):
 
     res = np.moveaxis(res, -1, 0)
     return res.astype(np.uint16)
+
+
+def fft(signal, sampling_rate):
+    """
+    Returns the dominant frequency of each pixel across all the frames
+    @:arg:
+    signal: np array with image data
+    :return: 2D np array containing the dominant frequency value for each pixel
+    """
+    num_frames, height, width = signal.shape
+    fft_frames = np.fft.fft(signal, axis=0)
+    fft_abs = np.abs(fft_frames)  # Magnitude of the frequency
+    fft_abs[..., 0] = 0  # Remove zero frequency component
+    dominant_frequencies = np.argmax(fft_abs, axis=0)
+    dominant_frequencies = dominant_frequencies * sampling_rate / signal.shape[0]
+    return dominant_frequencies
