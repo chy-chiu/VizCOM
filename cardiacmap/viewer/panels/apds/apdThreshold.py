a54e5254859c3d0d183f8c63859b9ec8779dba7d
Grayson Molesworth <gmolesworth3@gatech.edu>
2025-04-14 14:20:40 -0700

    Convert APD contour output to RGB. Allow custom colors for contours.

--- cardiacmap/viewer/panels/apds/apdThreshold.py
+++ cardiacmap/viewer/panels/apds/apdThreshold.py
@@ -51,18 +51,16 @@ class APDThresholdWindow(QMainWindow):
             xRange=(-VIEWPORT_MARGIN, IMAGE_SIZE + VIEWPORT_MARGIN),
             yRange=(-VIEWPORT_MARGIN, IMAGE_SIZE + VIEWPORT_MARGIN),
         )
-        self.output_view = pg.ImageView()
+        self.output_view = pg.ImageView(levelMode='rgba')
         self.output_view.setImage(self.data[imgIdx])
         self.output_view.view.setMouseEnabled(False, False)
+
 
         self.output_view.view.setRange(
             xRange=(-VIEWPORT_MARGIN, IMAGE_SIZE + VIEWPORT_MARGIN),
             yRange=(-VIEWPORT_MARGIN, IMAGE_SIZE + VIEWPORT_MARGIN),
         )
 
-        #self.image_views = QTabWidget()
-        #self.image_views.addTab(self.image_view, "Image")
-        #self.image_views.addTab(self.output_view, "Output")
         self.image_views = QHBoxLayout()
         self.image_views.addWidget(self.image_view)
         self.image_views.addWidget(self.output_view)
@@ -72,13 +70,10 @@ class APDThresholdWindow(QMainWindow):
         self.image_view.ui.menuBtn.hide()
         self.output_view.ui.roiBtn.hide()
         self.output_view.ui.menuBtn.hide()
-
-        #self.image_view.view.showAxes(False)
-        #self.image_view.view.invertY(True)
+        self.output_view.ui.histogram.hide()
 
         cm = pg.colormap.get("nipy_spectral", source="matplotlib")
         self.image_view.setColorMap(cm)
-        self.output_view.setColorMap(cm)
 
         img_layout.addLayout(self.image_views)
 
@@ -94,6 +89,7 @@ class APDThresholdWindow(QMainWindow):
         self.options_widget = QWidget()
         layout = QVBoxLayout()
         self.options_1 = QToolBar()
+        self.options_2 = QToolBar()
         self.actions_bar = QToolBar()
 
         self.threshold = Spinbox(
@@ -108,10 +104,11 @@ class APDThresholdWindow(QMainWindow):
             min_width=70,
             max_width=70,
         )
+        self.threshold_color_button = pg.ColorButton(color = (255, 255, 255))
         self.kernel_width = Spinbox(
-            min=0,
+            min=1,
             max=64,
-            val=2,
+            val=1,
             step=1,
             min_width=70,
             max_width=70,
@@ -121,15 +118,21 @@ class APDThresholdWindow(QMainWindow):
         self.options_1.addWidget(self.threshold)
         self.options_1.addWidget(QLabel("Beat: "))
         self.options_1.addWidget(self.start_frame)
-        self.options_1.addWidget(QLabel("Kernel Width: "))
-        self.options_1.addWidget(self.kernel_width)
+        self.options_1.addWidget(QLabel("Threshold Color:"))
+        self.options_1.addWidget(self.threshold_color_button)
+        self.options_2.addWidget(QLabel("Noise Removal Kernel Size (1 = disabled): "))
+        self.options_2.addWidget(self.kernel_width)
 
         self.options_1.setStyleSheet(QTOOLBAR_STYLE)
+        self.options_2.setStyleSheet(QTOOLBAR_STYLE)
 
         self.start_frame.valueChanged.connect(self.update_keyframe)
         self.kernel_width.valueChanged.connect(self.update_threshold)
+        self.threshold_color_button.sigColorChanged.connect(self.update_threshold)
 
         layout.addWidget(self.options_1)
+        layout.addSpacing(5)
+        layout.addWidget(self.options_2)
         layout.addSpacing(5)
         layout.addWidget(self.actions_bar)
 
@@ -144,30 +147,41 @@ class APDThresholdWindow(QMainWindow):
         start_frame = int(self.start_frame.value())
         kernel_width = int(self.kernel_width.value())
         threshMask = threshold(self.data[start_frame], self.threshold.value())
-        outputImage = self.data[start_frame] * threshMask
 
         if kernel_width > 1:
             kernel = np.ones((kernel_width, kernel_width))
             # opening
-            outputImage = cv2.erode(outputImage, kernel)
-            outputImage = cv2.dilate(outputImage, kernel)
+            threshMask = cv2.erode(threshMask, kernel)
+            threshMask = cv2.dilate(threshMask, kernel)
 
             # closing
-            outputImage = cv2.dilate(outputImage, kernel)
-            outputImage = cv2.erode(outputImage, kernel)
-
-        self.output_view.setImage(outputImage)
+            threshMask = cv2.dilate(threshMask, kernel)
+            threshMask = cv2.erode(threshMask, kernel)
+        
+        # mask grayscale image
+        outputImage = self.data[start_frame] * threshMask
 
         # contouring 
         contours = find_contours(outputImage, self.threshold.value())
+
+        # normalize, [0-511]
+        levels = self.image_view.ui.histogram.item.getLevels()
+        outputImage -= levels[0]
+        outputImage /= levels[1]
+        outputImage *= 511
+        outputImage[outputImage > 511] = 511
+
+        # convert to color
+        lut = self.image_view.getHistogramWidget().gradient.colorMap().getLookupTable()
+        outputImage = np.take(lut, outputImage.astype(np.uint16), axis=0)
+
+        # add color contours
         for contour in contours:
             for p in contour:
-                outputImage[int(p[0]), int(p[1])] = self.data[start_frame].max()
+                outputImage[int(p[0]), int(p[1]), :] = self.threshold_color_button.color().getRgb()[:3]
 
-        # set levels
-        levels = self.image_view.ui.histogram.item.getLevels()
-        self.output_view.setLevels(levels[0], levels[1])
-        self.output_view.ui.histogram.item.setHistogramRange(levels[0], levels[1])
+        # set image
+        self.output_view.setImage(outputImage)
 
     def update_keyframe(self):
         levels = self.image_view.ui.histogram.item.getLevels()
