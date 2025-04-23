383fd19a7bd5308f3a9f21c1256d7a681b8b0762
Grayson Molesworth <gmolesworth3@gatech.edu>
2025-04-22 20:10:09 -0700

    Allow Multiple APD/DI Thresholds

--- cardiacmap/viewer/panels/apds/apdThreshold.py
+++ cardiacmap/viewer/panels/apds/apdThreshold.py
@@ -87,98 +87,107 @@ class APDThresholdWindow(QMainWindow):
 
     def init_options(self):
         self.options_widget = QWidget()
-        layout = QVBoxLayout()
+        self.options_layout = QVBoxLayout()
         self.options_1 = QToolBar()
-        self.options_2 = QToolBar()
-        self.actions_bar = QToolBar()
 
-        self.threshold = Spinbox(
-            min=0, max=1000, val=25, step=1, min_width=60, max_width=60
+        self.num_thresholds = Spinbox(
+            min=1, max=100, val=1, step=1, min_width=60, max_width=60
         )
-        self.threshold.valueChanged.connect(self.update_threshold)
+
         self.start_frame = Spinbox(
             min=self.initFrame,
-            max=1000, # SET MAX TO NUMBER OF BEATS
+            max=100000, # SET MAX TO NUMBER OF BEATS
             val=0,
             step=1,
             min_width=70,
             max_width=70,
         )
-        self.threshold_color_button = pg.ColorButton(color = (255, 255, 255))
-        self.kernel_width = Spinbox(
-            min=1,
-            max=64,
-            val=1,
-            step=1,
-            min_width=70,
-            max_width=70,
-        )
 
-        self.options_1.addWidget(QLabel("Threshold: "))
-        self.options_1.addWidget(self.threshold)
+        self.contour_list_layout = QVBoxLayout()
+        self.contour_list = []
+
+        # set listeners
+        self.start_frame.valueChanged.connect(self.update_keyframe)
+        self.num_thresholds.valueChanged.connect(self.update_threshold)
+
+        # set up toolbars
         self.options_1.addWidget(QLabel("Beat: "))
         self.options_1.addWidget(self.start_frame)
-        self.options_1.addWidget(QLabel("Threshold Color:"))
-        self.options_1.addWidget(self.threshold_color_button)
-        self.options_2.addWidget(QLabel("Noise Removal Kernel Size (1 = disabled): "))
-        self.options_2.addWidget(self.kernel_width)
+
+        self.options_1.addWidget(QLabel("# of Contours: "))
+        self.options_1.addWidget(self.num_thresholds)
 
         self.options_1.setStyleSheet(QTOOLBAR_STYLE)
-        self.options_2.setStyleSheet(QTOOLBAR_STYLE)
 
-        self.start_frame.valueChanged.connect(self.update_keyframe)
-        self.kernel_width.valueChanged.connect(self.update_threshold)
-        self.threshold_color_button.sigColorChanged.connect(self.update_threshold)
-
-        layout.addWidget(self.options_1)
-        layout.addSpacing(5)
-        layout.addWidget(self.options_2)
-        layout.addSpacing(5)
-        layout.addWidget(self.actions_bar)
+        # initialize layout
+        self.options_layout.addWidget(self.options_1)
+        self.options_layout.addSpacing(5)
 
         self.update_threshold()
-        self.options_widget.setLayout(layout)
+        self.options_widget.setLayout(self.options_layout)
 
 
     def update_threshold_marker(self):
         self.threshold.setValue(round(self.signal_panel.threshold_marker.getYPos(), 2))
 
+    def update_contour_list(self):
+        num = int(self.num_thresholds.value())
+        listLen = len(self.contour_list)
+        diff = listLen - num
+        if diff != 0:
+            if diff < 0:
+                #add threshold
+                for i in range(-diff):
+                    cli = self.ContourListItem(self)
+                    self.contour_list.append(cli)
+
+                    if len(self.contour_list) == 1:
+                            self.threshold = cli.threshold
+                            self.threshold_color_button = cli.color_button
+
+                    self.options_layout.addWidget(cli)
+            else:
+                #remove threshold
+                for i in range(diff):
+                    cli = self.contour_list[-1]
+                    self.contour_list.remove(cli)
+                    self.options_layout.removeWidget(cli)
+                    cli.delete()
+
     def update_threshold(self):
+        self.update_contour_list()
         start_frame = int(self.start_frame.value())
-        kernel_width = int(self.kernel_width.value())
-        threshMask = threshold(self.data[start_frame], self.threshold.value())
 
-        if kernel_width > 1:
-            kernel = np.ones((kernel_width, kernel_width))
-            # opening
-            threshMask = cv2.erode(threshMask, kernel)
-            threshMask = cv2.dilate(threshMask, kernel)
-
-            # closing
-            threshMask = cv2.dilate(threshMask, kernel)
-            threshMask = cv2.erode(threshMask, kernel)
-        
-        # mask grayscale image
-        outputImage = self.data[start_frame] * threshMask
+        outputImage = np.copy(self.data[start_frame])
 
         # contouring 
-        contours = find_contours(outputImage, self.threshold.value())
+        c = []
+        for i in range(int(self.num_thresholds.value())):
+            contours = find_contours(outputImage, self.contour_list[i].threshold.value())
+            c.append(contours)
 
         # normalize, [0-511]
         levels = self.image_view.ui.histogram.item.getLevels()
         outputImage -= levels[0]
-        outputImage /= levels[1]
+        outputImage /= levels[1] - levels[0]
         outputImage *= 511
         outputImage[outputImage > 511] = 511
+        outputImage[outputImage < 0] = 0
+        outputImage = outputImage.astype(np.uint16)
+
+        #print(outputImage.min(), outputImage.max())
 
         # convert to color
         lut = self.image_view.getHistogramWidget().gradient.colorMap().getLookupTable()
-        outputImage = np.take(lut, outputImage.astype(np.uint16), axis=0)
+        outputImage = np.take(lut, outputImage, axis=0)
 
         # add color contours
-        for contour in contours:
-            for p in contour:
-                outputImage[int(p[0]), int(p[1]), :] = self.threshold_color_button.color().getRgb()[:3]
+        for i in range(int(self.num_thresholds.value())):
+            contours = c[i]
+            for contour in contours:
+                for p in contour:
+                    #print(self.contour_list[i].color_button.color().getRgb()[:3])
+                    outputImage[int(p[0]), int(p[1]), :] = self.contour_list[i].color_button.color().getRgb()[:3]
 
         # set image
         self.output_view.setImage(outputImage)
@@ -199,5 +208,41 @@ class APDThresholdWindow(QMainWindow):
         self.image_view.ui.histogram.item.blockSignals(False)
 
         self.update_threshold()
-        
+
+    class ContourListItem(QWidget):
+        def  __init__(self, parent):
+            QWidget.__init__(self)
+
+            self.parent = parent
+
+            self.threshold = Spinbox(
+                min=0, max=1000, val=5*len(parent.contour_list), step=1, min_width=60, max_width=60
+            )
+            self.threshold.valueChanged.connect(parent.update_threshold)
+
+            self.color_button = pg.ColorButton(color = (255, 255, 255))
+            self.color_button.sigColorChanged.connect(parent.update_threshold)
+
+            self.t_label = QLabel("Threshold:")
+            self.tc_label = QLabel("Threshold Color:")
+
+            self.layout = QHBoxLayout()
+            self.layout.addWidget(self.t_label)
+            self.layout.addWidget(self.threshold)
+            self.layout.addWidget(self.tc_label)
+            self.layout.addWidget(self.color_button)
+            self.layout.addStretch()
+
+            self.setLayout(self.layout)
+
+        def delete(self):
+            self.color_button.setParent(None)
+            self.threshold.setParent(None)
+            self.t_label.setParent(None)
+            self.tc_label.setParent(None)
+            self.setLayout(None)
+            self.destroy()
+
+
+
         
